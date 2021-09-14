58dc2704f84cdb9fec685d7ea542f87c99c69998
Thomas Adams <tqsare@gmail.com>
2021-09-13 23:52:33 -0700

    add delete route

--- main.go
+++ main.go
@@ -3,65 +3,88 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 type Article struct {
-	Id string `json:"Id"`
-	Title string `json:"Title"`
-	Desc string `json:"desc"`
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
 	Content string `json:"content"`
 }
 
 var Articles []Article
 
-func allArticles(w http.ResponseWriter, r *http.Request){
+func allArticles(w http.ResponseWriter, r *http.Request) {
 	articles := []Article{
-		Article{Title:"Test Title", Desc: "Test Description", Content: "Test Content"},
-		Article{Title:"Test Title2", Desc: "Test Description2", Content: "Test Content2"},
+		Article{Title: "Test Title", Desc: "Test Description", Content: "Test Content"},
+		Article{Title: "Test Title2", Desc: "Test Description2", Content: "Test Content2"},
 	}
 	fmt.Println("Endpoint Hit: All Articels")
 	json.NewEncoder(w).Encode(articles)
 }
 
-func homePage(w http.ResponseWriter, r *http.Request)  {
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Homepage Endpoint Hit")
 }
 
-func returnSingleArticle(w http.ResponseWriter, r *http.Request){
-    vars := mux.Vars(r)
-    key := vars["id"]
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
 
-    for _, article := range Articles {
-        if article.Id == key {
-            json.NewEncoder(w).Encode(article)
-        }
-    }
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
 
 }
 
 func createNewArticle(w http.ResponseWriter, r *http.Request) {
-    // get the body of our POST request
-    // return the string response containing the request body    
-    reqBody, _ := ioutil.ReadAll(r.Body)
-    fmt.Fprintf(w, "%+v", string(reqBody))
+	// get the body of our POST request
+	// return the string response containing the request body
+	reqBody, _ := io.ReadAll(r.Body)
+	fmt.Fprintf(w, "%+v", string(reqBody))
 }
 
-func handleRequest()  {
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	// once again, we will need to parse the path parameters
+	vars := mux.Vars(r)
+	// we will need to extract the `id` of the article we
+	// wish to delete
+	id := vars["id"]
+
+	// we then need to loop through all our articles
+	for index, article := range Articles {
+		// if our id path parameter matches one of our
+		// articles
+		if article.Id == id {
+			// updates our Articles array to remove the
+			// article
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+
+}
+
+func handleRequest() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func handleRequests() {
-	myRouter:= mux.NewRouter().StrictSlash(true)
+	myRouter := mux.NewRouter().StrictSlash(true)
 
 	myRouter.HandleFunc("/", homePage)
-	
-	myRouter.HandleFunc("/all", allArticles )
 
+	myRouter.HandleFunc("/all", allArticles)
+
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
 	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8082", myRouter))
@@ -69,8 +92,8 @@ func handleRequests() {
 
 func main() {
 	Articles = []Article{
-        Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-        Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-    }
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
 	handleRequests()
 }
